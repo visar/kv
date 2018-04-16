9cae9ae1807628fe2fc2c4d8bd1c898b5775b4d4
Visar Zejnullahu <visar.zejnullahu@gmail.com>
2018-04-16 09:36:21 +0200

    Remove buckets on exit

--- test/kv/registry_test.exs
+++ test/kv/registry_test.exs
@@ -18,4 +18,12 @@ defmodule KV.RegistryTest do
     Bucket.put(bucket, "milk", 1)
     assert Bucket.get(bucket, "milk") == 1
   end
+
+  test "remove buckets on exit", %{registry: registry} do
+    Registry.create(registry, "shopping")
+    {:ok, bucket} = Registry.lookup(registry, "shopping")
+    Agent.stop(bucket)
+
+    assert Registry.lookup(registry, "shopping") == :error
+  end
 end
